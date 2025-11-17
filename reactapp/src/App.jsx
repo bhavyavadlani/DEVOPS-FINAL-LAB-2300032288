f807465bb570ce950fcbc375f91d7b6a77c3023a
vadlanibhavya <vadlani.bhavya@gmail.com>
2025-11-17 10:18:18 +0530

    updated

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const BASE_URL = "http://localhost:2000/course";
+  const BASE_URL = "http://localhost:30071/course";
 
   const [courses, setCourses] = useState([]);
   const [newCourse, setNewCourse] = useState({
@@ -52,7 +52,7 @@ function App() {
       } else {
         setSearchResult(data);
       }
-    } catch (error) {
+    } catch {
       setSearchResult("NOT_FOUND");
     }
   };
